ca7b1cb8a31c886f02351006ff162cc72fe75f44
Marcin Węglowski <mcweglowski@gmail.com>
2021-03-25 22:50:39 +0100

    Code restructurisation

--- 992_Checkbox/App.js
+++ 992_Checkbox/App.js
@@ -1,24 +1,65 @@
-const PositiveMessage = () => <p>You can watch the movie</p>
-const NegativeMessage = () => <p>You can not watch the movie</p>
+const ValidationMessage = (props) => <p>{props.txt}</p>
 
-class CheckboxAgeConfirmation extends React.Component {
+
+const OrderForm = (props) => {
+    const {submit, checked, change} = props;
+
+    return (
+        <form onSubmit={submit}>
+            <input type="checkbox" id="age" onChange={change} checked={checked}/>
+            <label htmlFor="age">I am at least 16 years old</label>
+            <br/>
+            <button type="submit">Buy!</button>
+        </form>
+    )
+}
+
+class TicketShop extends React.Component {
     state = {
         isConfirmed: false,
+        isFormSubmitted: false,
     }
 
     handleCheckoboxChange = () => {
         this.setState({
             isConfirmed: !this.state.isConfirmed,
+            isFormSubmitted: false,
         })
     }
 
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!this.state.isFormSubmitted) {
+            this.setState({
+                isFormSubmitted: true,
+            })    
+        }
+    }
+
+    displayMessage = () => {
+        return this.state.isFormSubmitted ? 
+                this.state.isConfirmed ? 
+                    <ValidationMessage txt="You can watch the movie"/> : 
+                    <ValidationMessage txt="You can not watch the movie"/> : 
+                null
+    }
+
     render() {
+        const {
+            isConfirmed, 
+            isFormSubmitted
+        } = this.state
+
         return (
             <React.Fragment>
                 <h1>Buy a horror ticket!</h1>
-                <input type="checkbox" id="age" onChange={this.handleCheckoboxChange} checked={this.state.isConfirmed}/>
-                <label htmlFor="age">I am at least 16 years old</label>
-                {this.state.isConfirmed ? <PositiveMessage/> : <NegativeMessage/>}
+                <OrderForm 
+                    change={this.handleCheckoboxChange} 
+                    submit={this.handleFormSubmit} 
+                    checked={isConfirmed}
+                />
+                {this.displayMessage()}
             </React.Fragment>
         )
     }
@@ -31,4 +72,4 @@ class CheckboxAgeConfirmation extends React.Component {
 
 const header = <h1 className="title">Hello World!!!</h1>
 
-ReactDOM.render(<CheckboxAgeConfirmation/>, document.getElementById('root'))
+ReactDOM.render(<TicketShop/>, document.getElementById('root'))
