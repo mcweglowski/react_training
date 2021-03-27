dc59ac7d95eede3b929d9d0900527eac161ada49
Marcin Węglowski <mcweglowski@gmail.com>
2021-03-27 22:34:58 +0100

    Use object in list

--- 990_Arrays/App.js
+++ 990_Arrays/App.js
@@ -1,12 +1,40 @@
-const Item = (props) => <li>{`Fruit: ${props.content}`}</li>
+const data = {
+    users: [
+        {
+            id: 1,
+            age: 29,
+            name: "Anthony",
+        },
+        {
+            id: 2,
+            age: 49,
+            name: "Martha",
+        },
+        {
+            id: 3,
+            age: 19,
+            name: "Stephanie",
+        },
+        {
+            id: 4,
+            age: 21,
+            name: "Carl",
+        }
+    ]
+}
+
+const Item = (props) => (
+    <div>
+        <h1>{`User ${props.content.name}`}</h1>
+        <h2>{`Has ${props.content.age} years`}</h2>
+    </div>
+)
 
 class ListItems extends React.Component {
-    state = {
-        items: ["apple", "plum", "peach"]
-    }
-
     render() {
-        const Items = this.state.items.map((item, id) => <Item key={item} content={item}/>);
+        const users = this.props.data.users;
+        const Items = users.map(item => <Item key={item.id} content={item} />)
+
         return (
             <React.Fragment>
                 <ul>
@@ -17,4 +45,4 @@ class ListItems extends React.Component {
     }
 }
 
-ReactDOM.render(<ListItems/>, document.getElementById('root'))
+ReactDOM.render(<ListItems data={data} />, document.getElementById('root'))
